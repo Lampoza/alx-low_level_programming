9be8fddae301074c3588bbedb58582ec0db527e6
Lampoza <gqiranaluzuko@gmail.com>
2023-06-22 14:51:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checking uppercase character
+ * @c: Character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}

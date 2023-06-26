bd18b83792b2a16c277dc218d1cc2f8d812555a4
Lampoza <gqiranaluzuko@gmail.com>
2023-06-26 11:02:58 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it updates the value of the variable
+ * then the pointer points to to 98
+ * @n: is  pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * _putchar - it writes the character c to stdout
+ * @c: is the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno(ENOENT) is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
